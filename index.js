c4755eb91ef0de106be977e686f4f98c0f17b61b
Jared Patterson <jpwesley34@gmail.com>
2023-11-01 07:15:41 -0700

    does math

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (num1, num2) => num1 / num2;
+
+//const nums = [1,2,3];
+//const square = nums.map(x => x ** 2);
+const square = num3 => num3 * num3;
+
+const add = (num4, num5) => num4 + num5;
